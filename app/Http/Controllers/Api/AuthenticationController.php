01c4fd76ef2f8f87e9648200e6b935eea2f5ea0a
Erastus Doh <erastusdoh@gmail.com>
2020-06-04 19:52:43 +0000

    create user api

--- app/Http/Controllers/Api/AuthenticationController.php
+++ app/Http/Controllers/Api/AuthenticationController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use App\Traits\ApiBaseController;
+use phpseclib\Crypt\Hash;
 
 /**
  * @group Authentication Management
@@ -104,4 +105,65 @@ class AuthenticationController extends Controller
         $data['token'] = $user->createToken(env('APP_NAME'))->accessToken;
         return $data;
     }
-}
+
+
+    /**
+     * Register a User
+     *
+     * Registers a user as an entrepreneur or investor.
+     *
+     * @bodyParam first_name string required The first name of the user. Example: Jane
+     * @bodyParam last_name string required The last name of the user. Example: Doe
+     * @bodyParam email string required The email of the user. Example: mail@mail.com
+     * @bodyParam password string required The password of the user.
+     * @bodyParam password_confirmation string required The password confirmation for the password.
+     *
+     * @response 200 {
+     * "success": {
+     * "code": 200,
+     * "message": "Request completed successfully."
+     * },
+     * "data": {
+     * "id": 1,
+     * "first_name": "Jane",
+     * "last_name": "Doe",
+     * "email": "jane@doe.com",
+     * "token": "7geRI9P4LUFj3ensaxOV070Uk1yXeQ23ptqerJYc"
+     *    }
+     * }
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+        public function registerUser(Request $request)
+        {
+            //validate credentials
+            $validator = Validator::make($request->all(), [
+                'first_name' => 'required|string|max:255',
+                'last_name' => 'required|string|max:255',
+                'email' => 'required|email|max:255|unique:users',
+                'password' => 'required|confirmed'
+            ]);
+
+            //send validation error response if any
+            if ($validator->fails()) {
+                return $this->sendErrorResponse($validator->errors()->first());
+            }
+
+
+
+            //create the user
+            $user = User::query()->create([
+                'fname' =>$request->input('first_name'),
+                'lname' =>$request->input('last_name'),
+                'email' =>$request->input('email'),
+                'password' =>Hash::make($request->input('password'))
+            ]);
+
+
+            //data to be sent back
+            return $this->sendSuccessResponse($this->generateUserData($user));
+        }
+
+
+    }
